7a458891474184e56c75732670c2897b6572dc21
coderoque <coderoque@gmail.com>
2023-07-03 06:13:30 +0400

    add admin page for User and Task models

--- backend/task_manager/api/admin.py
+++ backend/task_manager/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created', 'updated', 'user')
 
 # Register your models here.
